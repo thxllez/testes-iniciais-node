8d09236d745c420ca506d5782663e05f7b4587f1
Thalles Trancoso <>
2018-12-12 23:06:33 -0200

    ajustes

--- config/server.js
+++ config/server.js
@@ -10,12 +10,13 @@ appConfig.set('views', './app/views'); //setando diretorio padrao de views
 
 appConfig.use(bodyParser.urlencoded({extended : true})); //implementando o body-parser no appConfig efetivamente
 //extended é uma propriedade para o bodyParser poder interpretar formulários mais complexos
-appConfig.use(expressValidator()); //implementando o body-parser no appConfig efetivamente
+appConfig.use(expressValidator()); //implementando o expressValidator no appConfig efetivamente
 
 consign()
 	.include('./app/routes') //faz o carregamento automatico de todas as rotas e inclui dentro de appConfig
 	.then('config/dbConnection.js') //faz o carregamento automatico do módulo de conexão com o bd e inclui dentro de appConfig
 	.then('./app/models') //faz o carregamento automatico dos models e inclui dentro de appConfig
+	.then('app/controllers') // faz o carregamento automatico das controllers e inclui dentro de appConfig
 	.into(appConfig); //carrega tudo dentro da variável appConfig, que será exportada para o servidor em app.js
 
 // O app agora contém todos os módulos carregados no consign
